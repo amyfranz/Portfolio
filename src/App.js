d481240a5294e95b96b9aabdbfd52da5957beb4d
Amy Franz <amy.franz@gmail.com>
2020-04-04 10:44:00 +0100

    testing netlify form submission

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import NavBar from "./components/navBar/navBar";
 import Home from "./components/home/home";
 import AboutMe from "./components/aboutMe/aboutMe";
@@ -6,13 +6,48 @@ import Portfolio from "./components/portfolio/portfolio";
 import Blog from "./components/blog/blog";
 import ContactMe from "./components/contactMe/contactMe";
 import Footer from "./components/footer/footer";
+import Axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar /> <Home /> <Portfolio /> <AboutMe /> <Blog /> <ContactMe />
-      <Footer />
-    </div>
-  );
+class App extends Component {
+  state = {
+    nameEmailForm: "",
+    emailEmailForm: "",
+    subjectEmailForm: "",
+    messageEmailForm: "",
+  };
+  render() {
+    return (
+      <div className="App">
+        <NavBar /> <Home /> <Portfolio /> <AboutMe /> <Blog />{" "}
+        <ContactMe
+          handleEmailFormChange={this.handleEmailFormChange}
+          // handleEmailFormSubmission={this.handleEmailFormSubmission}
+        />
+        <Footer />
+      </div>
+    );
+  }
+  handleEmailFormChange = (e) => {
+    const key = `${e.target.name}EmailForm`;
+    const value = e.target.value;
+    const obj = {};
+    obj[key] = value;
+    this.setState(obj);
+  };
+  // async handleEmailFormSubmission(e){
+  //   e.preventDefault();
+  //   const {
+  //     nameEmailForm,
+  //     emailEmailForm,
+  //     subjectEmailForm,
+  //     messageEmailForm,
+  //   } = this.state;
+  //   const form = await Axios.post('/api/form', {
+  //     name: nameEmailForm,
+  //     email: emailEmailForm,
+  //     subject: subjectEmailForm,
+  //     message: messageEmailForm
+  //   })
+  //};
 }
 export default App;
--- src/components/contactMe/contactMe.js
+++ src/components/contactMe/contactMe.js
@@ -1,17 +1,41 @@
 import React from "react";
 
-export default function contactMe() {
+export default function contactMe(props) {
   return (
     <div className="contactMe">
       <div className="contactMeTitle">
         <h1>contactMe</h1>
       </div>
       <div className="contactMeForm">
-        <form>
-          <input type="text" placeholder="Name:" name="name" required />
-          <input type="email" placeholder="Email:" name="email" required />
-          <input type="text" placeholder="Subject:" name="subject" required />
-          <textarea placeholder="Message:" name="message" required />
+        <form action="POST" data-netlify="true">
+          {/* onSubmit={props.handleEmailFormSubmission}> */}
+          <input
+            type="text"
+            placeholder="Name:"
+            name="name"
+            required
+            onChange={props.handleEmailFormChange}
+          />
+          <input
+            type="email"
+            placeholder="Email:"
+            name="email"
+            required
+            onChange={props.handleEmailFormChange}
+          />
+          <input
+            type="text"
+            placeholder="Subject:"
+            name="subject"
+            required
+            onChange={props.handleEmailFormChange}
+          />
+          <textarea
+            placeholder="Message:"
+            name="message"
+            required
+            onChange={props.handleEmailFormChange}
+          />
           <button type="submit">Submit</button>
         </form>
       </div>
